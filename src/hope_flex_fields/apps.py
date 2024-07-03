a9f64930e4bf01c4ebafd53ab428fca72301240a
saxix <s.apostolico@gmail.com>
2024-07-03 22:57:13 +0200

    fixes default fields creation

--- src/hope_flex_fields/apps.py
+++ src/hope_flex_fields/apps.py
@@ -1,6 +1,20 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
 from strategy_field.utils import fqn
+
+
+def create_default_fields(sender, **kwargs):
+    from hope_flex_fields.models import FieldDefinition
+    from hope_flex_fields.registry import field_registry
+    from hope_flex_fields.utils import get_kwargs_for_field
+
+    for fld in field_registry:
+        FieldDefinition.objects.get_or_create(
+            name=fld.__name__,
+            field_type=fqn(fld),
+            defaults={"attrs": get_kwargs_for_field(fld)},
+        )
 
 
 class Config(AppConfig):
@@ -8,13 +22,4 @@ class Config(AppConfig):
     verbose_name = "Flex Fields"
 
     def ready(self):
-        from hope_flex_fields.models import FieldDefinition
-        from hope_flex_fields.registry import field_registry
-        from hope_flex_fields.utils import get_kwargs_for_field
-
-        for fld in field_registry:
-            FieldDefinition.objects.get_or_create(
-                name=fld.__name__,
-                field_type=fqn(fld),
-                defaults={"attrs": get_kwargs_for_field(fld)},
-            )
+        post_migrate.connect(create_default_fields, sender=self)
