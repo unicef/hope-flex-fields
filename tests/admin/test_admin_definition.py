7df38598a342f3f67341c98bcaca255879097e1b
Domenico DiNicola <dom.dinicola@gmail.com>
2024-07-15 15:41:09 +0200

    updates

--- tests/admin/test_admin_definition.py
+++ tests/admin/test_admin_definition.py
@@ -40,9 +40,10 @@ def test_field_test(app, record):
 def test_fields_create(app, record):
     url = reverse("admin:hope_flex_fields_fielddefinition_add")
     res = app.get(url)
-    res.form["name"] = "int"
-    res.form["field_type"] = fqn(forms.ChoiceField)
-    res = res.form.submit()
+    form = res.forms["fielddefinition_form"]
+    form["name"] = "int"
+    form["field_type"] = fqn(forms.ChoiceField)
+    res = form.submit()
     assert res.status_code == 302
     obj: FieldDefinition = FieldDefinition.objects.get(name="int")
     assert obj.attrs == {
@@ -55,16 +56,19 @@ def test_fields_create(app, record):
 def test_fields_create_and_update(app, record):
     url = reverse("admin:hope_flex_fields_fielddefinition_add")
     res = app.get(url)
-    res.form["name"] = "Int"
-    res.form["field_type"] = fqn(forms.IntegerField)
-    res = res.form.submit("_continue").follow()
-    assert res.form["attrs"].value == (
-        '{"max_value": null, "min_value": null, ' '"required": false, "help_text": ""}'
+    form = res.forms["fielddefinition_form"]
+    form["name"] = "Int"
+    form["field_type"] = fqn(forms.IntegerField)
+    res = form.submit("_continue").follow()
+    form = res.forms["fielddefinition_form"]
+    assert form["attrs"].value == (
+        '{"max_value": null, "min_value": null, "step_size": null, '
+        '"required": false, "help_text": ""}'
     )
-    res.form["attrs"] = (
+    form["attrs"] = (
         '{"max_value": 1, "min_value": 10, ' '"required": false, "help_text": ""}'
     )
-    res.form.submit("_continue").follow()
+    form.submit("_continue").follow()
 
     obj: FieldDefinition = FieldDefinition.objects.get(name="Int")
     assert obj.attrs == {
@@ -72,15 +76,17 @@ def test_fields_create_and_update(app, record):
         "min_value": 10,
         "required": False,
         "help_text": "",
+        "step_size": None,
     }
 
 
 def test_fields_change_type(app, record):
     url = reverse("admin:hope_flex_fields_fielddefinition_change", args=[record.pk])
     res = app.get(url)
-    res.form["name"] = "Char"
-    res.form["field_type"] = fqn(forms.CharField)
-    res = res.form.submit("_continue")
+    form = res.forms["fielddefinition_form"]
+    form["name"] = "Char"
+    form["field_type"] = fqn(forms.CharField)
+    res = form.submit("_continue")
     assert res.status_code == 302, res.showbrowser()
     obj: FieldDefinition = FieldDefinition.objects.get(name="Char")
     assert obj.attrs == {
